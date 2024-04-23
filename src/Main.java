5b0770f395a210cf2f624681029a2e703b39a9ea
Yerassyl <es2010218@gmail.com>
2024-04-24 01:08:23 +0500

    The end of second assignment.

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,56 @@
+import java.util.Iterator;
+
 public class Main {
+    public static void main(String[] args) {
+        testArrayList();
+        testLinkedList();
+    }
+
+    private static void testArrayList() {
+        MyList<Integer> arrayList = new MyArrayList<>();
+        arrayList.add(10);
+        arrayList.add(20);
+        arrayList.add(30);
+
+        arrayList.addFirst(5);
+        arrayList.addLast(40);
+
+        arrayList.set(100, 2);
+
+        arrayList.removeFirst();
+        arrayList.removeLast();
+
+        arrayList.indexOf(20);
+        arrayList.lastIndexOf(20);
+
+        arrayList.exists(30);
+
+        Iterator<Integer> arrayListIterator = arrayList.iterator();
+        while (arrayListIterator.hasNext()) {
+            System.out.print(arrayListIterator.next() + " ");
+        }
+        System.out.println();
+    }
+
+    private static void testLinkedList() {
+        MyList<String> linkedList = new MyLinkedList<>();
+        linkedList.add("Hello");
+        linkedList.add("World");
+        linkedList.addFirst("Welcome");
+
+        linkedList.addLast("!");
+
+        linkedList.remove(1);
+
+        linkedList.indexOf("World");
+        linkedList.lastIndexOf("Hello");
+
+        linkedList.exists("Welcome");
+
+        Iterator<String> linkedListIterator = linkedList.iterator();
+        while (linkedListIterator.hasNext()) {
+            System.out.print(linkedListIterator.next() + " ");
+        }
+        System.out.println();
+    }
 }
--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,2 +1,172 @@
-public class MyArrayList {
+import java.util.Iterator;
+
+class MyArrayList<T> implements MyList<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object[] array;
+    private int size;
+
+    public MyArrayList() {
+        this.array = new Object[DEFAULT_CAPACITY];
+        this.size = 0;
+    }
+
+    @Override
+    public void add(T item) {
+        if (size == array.length) {
+            increaseCapacity();
+        }
+        array[size++] = item;
+    }
+
+    @Override
+    public void add(T item, int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        if (size == array.length) {
+            increaseCapacity();
+        }
+        System.arraycopy(array, index, array, index + 1, size - index);
+        array[index] = item;
+        size++;
+    }
+
+    @Override
+    public void addFirst(T item) {
+        add(item, 0);
+    }
+
+    @Override
+    public void addLast(T item) {
+        add(item);
+    }
+
+    @Override
+    public void set(T item, int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        array[index] = item;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        return (T) array[index];
+    }
+
+    @Override
+    public T getFirst() {
+        if (size == 0) {
+            throw new IllegalStateException("List is empty");
+        }
+        return (T) array[0];
+    }
+
+    @Override
+    public T getLast() {
+        if (size == 0) {
+            throw new IllegalStateException("List is empty");
+        }
+        return (T) array[size - 1];
+    }
+
+    @Override
+    public void remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        System.arraycopy(array, index + 1, array, index, size - index - 1);
+        array[--size] = null;
+    }
+
+    @Override
+    public void removeFirst() {
+        remove(0);
+    }
+
+    @Override
+    public void removeLast() {
+        if (size == 0) {
+            throw new IllegalStateException("List is empty");
+        }
+        array[--size] = null;
+    }
+
+    @Override
+    public void sort() {
+        // TODO: Implement sorting
+    }
+
+    @Override
+    public int indexOf(T item) {
+        for (int i = 0; i < size; i++) {
+            if (array[i].equals(item)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(T item) {
+        for (int i = size - 1; i >= 0; i--) {
+            if (array[i].equals(item)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public boolean exists(T item) {
+        return indexOf(item) != -1;
+    }
+
+    @Override
+    public T[] toArray() {
+        return (T[]) array.clone();
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            array[i] = null;
+        }
+        size = 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private int currentIndex = 0;
+
+            @Override
+            public boolean hasNext() {
+                return currentIndex < size;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new IllegalStateException("No more elements");
+                }
+                return (T) array[currentIndex++];
+            }
+        };
+    }
+
+    private void increaseCapacity() {
+        int newCapacity = array.length * 2;
+        Object[] newArray = new Object[newCapacity];
+        System.arraycopy(array, 0, newArray, 0, size);
+        array = newArray;
+    }
 }
